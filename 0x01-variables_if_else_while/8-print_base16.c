9324cd7b5ccd5e270c96d37408a22f2e192a606a
Astounagib <you@example.com>
2022-12-10 21:35:03 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - printing a-z
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
