ecaf6592665d40e27d92dcb3953840dc3d981698
Astounagib <you@example.com>
2022-12-10 21:14:14 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - printing a-z
+ *
+ * Return:0(sucess)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
