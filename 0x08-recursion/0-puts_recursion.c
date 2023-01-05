24ec643720b67f41560968781148443924c1435c
Astounagib <you@example.com>
2023-01-04 18:01:24 -0800

    Recursion task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
-{
-_putchar(s[0]);
-_puts_recursion(s + 1);
-}
-else
+if (s[0] == '\0')
 {
 _putchar('\n');
+return;
 }
+
+_putchar(s[0]);
+_puts_recursion(s + 1);
 }
