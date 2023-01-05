b482c936c1c5ee776aaa366c5219fffbc6102367
Astounagib <you@example.com>
2023-01-04 19:50:54 -0800

    Recursion task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,45 @@
-#include "main.h"
+#include "holberton.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to be used
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
  *
- * Return: the square root of n
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+
+int find_sqrt(int num, int root)
+{
+if ((root * root) == num)
+return (root);
+
+if (root == num / 2)
+return (-1);
+
+return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
 
 int _sqrt_recursion(int n)
 {
-if (n == 0 || n == 1)
-return (n);
-return (_sqrt(0, n));
-}
+int root = 0;
 
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- *
- * Return: the square root of n
- */
-
-int _sqrt(int n, int x)
-{
-if (n > x / 2)
+if (n < 0)
 return (-1);
-else if (n * n == x)
-return (n);
-return (_sqrt(n + 1, x));
+
+if (n == 1)
+return (1);
+
+return (find_sqrt(n, root));
 }
